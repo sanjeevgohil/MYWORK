432784cccba2a578e9a5eeeb0f3caa35daaeef21
Sanjeev Gohil <118007831+sanjeevgohil@users.noreply.github.com>
2024-04-30 08:39:45 +0530

    Add files via upload

---
+++ A7.java
@@ -0,0 +1,34 @@
+import java.sql.*;
+class A7
+{
+	public static void main(String []args)
+	{	
+		System.out.println("Welcome To J2EE");	
+		try
+		{
+			// For Class Driver 
+			Class.forName("net.ucanaccess.jdbc.UcanaccessDriver");
+	
+			//For Connection 
+			Connection cn = DriverManager.getConnection("jdbc:ucanaccess://D:/JDBC/aa.accdb");	
+
+			//For Statement Class
+			Statement st = cn.createStatement();
+
+			st.execute("update stud set name='Raj' where id=4");
+			st.execute("select * from stud"); 	
+
+			//For Preparing Resultset 
+			ResultSet rs = st.getResultSet();
+
+			System.out.println("ID"+"  "+"Name"+"  "+"City");
+			while(rs.next()){
+				System.out.println(rs.getInt(1) + "    " + rs.getString(2)+ "    " + rs.getString(3));
+			}			
+			
+		}
+		catch(Exception e){
+			System.out.println(e);
+		}
+	}
+}
---
+++ A8.java
@@ -0,0 +1,34 @@
+import java.sql.*;
+class A8
+{
+	public static void main(String []args)
+	{	
+		System.out.println("Welcome To J2EE");	
+		try
+		{
+			// For Class Driver 
+			Class.forName("net.ucanaccess.jdbc.UcanaccessDriver");
+	
+			//For Connection 
+			Connection cn = DriverManager.getConnection("jdbc:ucanaccess://D:/JDBC/aa.accdb");	
+
+			//For Statement Class
+			Statement st = cn.createStatement();
+
+			st.execute("delete from stud where id=8");
+			st.execute("select * from stud"); 	
+
+			//For Preparing Resultset 
+			ResultSet rs = st.getResultSet();
+
+			System.out.println("ID"+"  "+"Name"+"  "+"City");
+			while(rs.next()){
+				System.out.println(rs.getInt(1) + "    " + rs.getString(2)+ "    " + rs.getString(3));
+			}		
+			
+		}
+		catch(Exception e){
+			System.out.println(e);
+		}
+	}
+}
